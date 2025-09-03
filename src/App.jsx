3999a3e37b20ff2d357977988dd12204d02d2f8b
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-09-03 12:59:18 -0500

    Added die componenet with base prop value, and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import Die from "./components/Die";
 
 function App() {
   return (
     <main>
-      <h1></h1>
+      <h1>Tenzies</h1>
+      <p className="game-instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="grid-container">
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+      </div>
+      <button className="roll-button">Roll</button>
     </main>
   );
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+import "./Die.css";
+
+function Die(props) {
+  return <button className="die">{props.value}</button>;
+}
+
+export default Die;
