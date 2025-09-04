5517a63811948d801bfbca5243cd5198625ae5cf
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-09-04 15:28:23 -0500

    Added confetti library and logic for when player wins the game

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 import "./App.css";
 import Die from "./components/Die";
+import Confetti from "react-confetti";
 
 function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
+
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
 
   function generateAllNewDice() {
     return Array.from({ length: 10 }, () => ({
@@ -22,6 +27,10 @@ function App() {
           : { ...die, value: Math.ceil(Math.random() * 6) };
       })
     );
+  }
+
+  function newGame() {
+    setDice(generateAllNewDice());
   }
 
   function hold(id) {
@@ -44,14 +53,15 @@ function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1>Tenzies</h1>
       <p className="game-instructions">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
       <div className="grid-container">{diceElements}</div>
-      <button onClick={rollDice} className="roll-button">
-        Roll
+      <button onClick={gameWon ? newGame : rollDice} className="roll-button">
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
   );
